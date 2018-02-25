40dbb5423db822672dc7124b826fd7b633646f3d
Vineet Kosaraju <vineetsk1@gmail.com>
2018-02-24 23:47:19 -0800

    Stashing changes

---
+++ forecasting/SocialForces/vineet/utils.py
@@ -0,0 +1,9 @@
+def cart2pol(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return(rho, phi)
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return(x, y)
